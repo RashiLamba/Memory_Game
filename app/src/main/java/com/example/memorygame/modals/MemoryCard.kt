17582f841c883400a45467e134304ec51210a575
RashiLamba <rashi@reinotechnologies.com>
2021-12-06 05:39:52 +0530

    Game logic done for flipping and updating the moves and pairs matched

--- app/src/main/java/com/example/memorygame/modals/MemoryCard.kt
+++ app/src/main/java/com/example/memorygame/modals/MemoryCard.kt
@@ -2,6 +2,6 @@ package com.example.memorygame.modals
 
 data class MemoryCard(
     val identifier: Int,
-    val isFaceUp: Boolean = false,
-    val isMatched: Boolean = false
+    var isFaceUp: Boolean = false,
+    var isMatched: Boolean = false
 )
--- app/src/main/java/com/example/memorygame/modals/MemoryGame.kt
+++ app/src/main/java/com/example/memorygame/modals/MemoryGame.kt
@@ -5,7 +5,10 @@ import com.example.memorygame.utils.DEFAULT_ICONS
 class MemoryGame(private val boardSize: BoardSize) {
 
     val cards: List<MemoryCard>
-    val numPairsFound= 0
+    var numPairsFound = 0
+    private var numCardFlips  = 0
+    private var indexOfSingleSelectedCard: Int? = null
+
 
     init {
     val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
@@ -13,4 +16,60 @@ class MemoryGame(private val boardSize: BoardSize) {
     cards = randomizedImages.map { MemoryCard(it) }
     }
 
+    fun flipCard(position: Int): Boolean {
+        numCardFlips++
+        val card = cards[position]
+        var matchFound = false
+        //three cases:
+        /**
+         * 1. 0 cards previously flipped : restore cards + flip over the selected cards
+         * 2. 1 cards previously flipped : flip over the selected cards + check if the cards matches
+         * 3. 2 cards previously flipped : restore cards + flip over the selected cards
+         * */
+        if (indexOfSingleSelectedCard == null){
+            //0 or 2 cards are previously flipped card
+            restoreCards()
+            indexOfSingleSelectedCard = position
+        }else{
+            //exactly 1 card is previously flipped over
+            matchFound = checkForMatch(indexOfSingleSelectedCard!!, position)
+            indexOfSingleSelectedCard = null
+        }
+          card.isFaceUp = !card.isFaceUp
+        return  matchFound
+    }
+
+    private fun checkForMatch(position1: Int, position2: Int):Boolean {
+        return when {
+            cards[position1].identifier != cards[position2].identifier -> {
+                false
+            }
+            else -> {
+                cards[position1].isMatched = true
+                cards[position2].isMatched = true
+                numPairsFound++
+                true
+            }
+        }
+    }
+
+    private fun restoreCards() {
+        for (card in cards){
+            if (!card.isMatched){
+                card.isFaceUp = false
+            }
+        }
+    }
+
+    fun haveWonGame(): Boolean {
+        return numPairsFound == boardSize.getNumPairs()
+    }
+
+    fun isCardFaceUp(position: Int): Boolean {
+        return cards[position].isFaceUp
+    }
+
+    fun getNumMoves(): Int {
+        return numCardFlips / 2
+    }
 }
